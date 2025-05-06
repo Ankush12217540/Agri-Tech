2d21aa30651d2803b0d71dc645b3c747ac48d62b
Ankush Kumar <floatankush@gmail.com>
2025-05-06 23:52:00 +0530

    revised

--- agri-assessment/app/Models/Well.php
+++ agri-assessment/app/Models/Well.php
@@ -9,6 +9,8 @@ class Well extends Model
     protected $fillable = ['land_holding_id', 'depth', 'type'];
 
     public function landHolding()
+
+    
     {
         return $this->belongsTo(LandHolding::class);
     }
